9d1562f793c3f91cf1364169aa563869984566ef
Rémy Marronnier <rmarronnier@gmail.com>
2019-08-10 01:56:41 +0200

    1st commit - WIP

---
+++ README.md
@@ -0,0 +1,39 @@
+# franca
+
+Crystal port of [franc](https://github.com/wooorm/franc)
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     franca:
+       github: rmarronnier/franca
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "franca"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/rmarronnier/franca/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Rémy Marronnier](https://github.com/rmarronnier) - creator and maintainer
---
+++ src/franca.cr
@@ -0,0 +1,82 @@
+require "cadmium"
+require "json"
+
+module Franca
+  VERSION = "0.1.0"
+
+  @@expressions : Tuple(Symbol, Regex)
+
+  @@languages : Hash(Symbol, Array(String))
+
+  @@data : Hash(String, Array(Hash(String, String))) = File.read("../data/data.json").from_json.to_h
+
+  def detect(text : String) : String
+    detect_all(text)[0][0]
+  end
+
+  def detect_all(text : String) : Array(Array(String, Int32))
+    expression = get_top_expression(text, @@expressions)
+    return single_language_tuples(expression[0]) if !@@data.includes?(expression[0])
+    normalize(text, get_distances(Cadmium.ngrams.new.trigrams(text), data[expression[0]]))
+  end
+
+  # Create a single tuple as a list of tuples from a given
+  # language code. ???????????????????????????????
+  def single_language_tuples(language)
+    [[language, 1]]
+  end
+
+  def normalize(text : String, distances : Array(Hash(String, Int32)))
+    min = distances[0].value
+    max = text.size * 300 - min
+
+    distances.each do |distance|
+      distances[distance] = 1 - (distances[distance] - min) / max || 0
+    end
+    distances
+  end
+
+  def get_occurence(text : String, expression : Regex) : Float
+    count = text.match(expression)
+    (count ? count.size : 0) / value.size || 0
+  end
+
+  def get_top_expression(text : String, expressions = @@expressions) : Hash(Regex, Int32)
+    top_count = -1
+    top_expression : Regex
+    expressions.each do |expression|
+      count = get_occurrence(value, expression)
+      if (count > top_count)
+        top_count = count
+        top_expression = expression
+      end
+    end
+    {top_expression => top_count}
+  end
+
+  def get_distances(trigrams : Array(Tuple(String, Int32)), languages = @@languages) : Array(Hash(String, Int32))
+    distances : Array(Hash(String, Int32))
+    languages.each do |language|
+      distances << {language => get_distance(trigrams, language)}
+    end
+  end
+
+  def get_distance(trigrams, model : Array(String)) : Int32
+    distance = 0
+    difference : Int32
+
+    trigrams.each do |trigram|
+      if model.includes?(trigram.key)
+        difference = trigram.value - model[trigram.key] - 1
+        if difference < 0
+          difference = -difference
+        end
+      else
+        difference = 300 # max_difference
+      end
+      distance += difference
+    end
+  end
+
+  distance
+end
