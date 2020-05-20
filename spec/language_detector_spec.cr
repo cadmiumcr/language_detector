9a4e676e101b62590e31340cea908866cec50b7e
Chris Watson <cawatson1993@gmail.com>
2020-05-19 23:53:14 -0600

    Fixed a lot, still some more to do

--- spec/language_detector_spec.cr
+++ spec/language_detector_spec.cr
@@ -1,95 +1,16 @@
+require "json"
 require "./spec_helper"
+
 describe Cadmium::LanguageDetector do
   subject = Cadmium::LanguageDetector.new
-  test = "제 26 조
-  1) 모든 사람은 교육을 받을 권리를 가진다 . 교육은 최소한 초등 및 기초단계에서는 무상이어야 한다. 초등교육은 의무적이어야 한다. 기술 및 직업교육은 일반적으로 접근이 가능하여야 하며, 고등교육은 모든 사람에게 실력에 근거하여 동등하게 접근 가능하여야 한다.
-  2) 교육은 인격의 완전한 발전과 인권과 기본적 자유에 대한 존중의 강화를 목표로 한다. 교육은 모든 국가 , 인종 또는 종교 집단간에 이해, 관용 및 우의를 증진하며 , 평화의 유지를 위한 국제연합의 활동을 촉진하여야 한다.
-  3) 부모는 자녀에게 제공되는 교육의 종류를 선택할 우선권을 가진다 .
-  제 27 조
-  1) 모든 사람은 공동체의 문화생활에 자유롭게 참여하며 예술을 향유하고 과학의 발전과 그 혜택을 공유할 권리를 가진다 .
-  2) 모든 사람은 자신이 창작한 과학적 , 문학적 또는 예술적 산물로부터 발생하는 정신적, 물질적 이익을 보호받을 권리를 가진다 ."
 
-  test2 = "Rappelle-toi Barbara
-  Il pleuvait sans cesse sur Brest ce jour-là
-  Et tu marchais souriante
-  Épanouie ravie ruisselante
-  Sous la pluie
-  Rappelle-toi Barbara
-  Il pleuvait sans cesse sur Brest
-  Et je t'ai croisée rue de Siam
-  Tu souriais
-  Et moi je souriais de même
-  Rappelle-toi Barbara
-  Toi que je ne connaissais pas
-  Toi qui ne me connaissais pas
-  Rappelle-toi
-  Rappelle-toi quand même jour-là
-  N'oublie pas
-  Un homme sous un porche s'abritait
-  Et il a crié ton nom
-  Barbara
-  Et tu as couru vers lui sous la pluie
-  Ruisselante ravie épanouie
-  Et tu t'es jetée dans ses bras
-  Rappelle-toi cela Barbara
-  Et ne m'en veux pas si je te tutoie
-  Je dis tu à tous ceux que j'aime
-  Même si je ne les ai vus qu'une seule fois
-  Je dis tu à tous ceux qui s'aiment
-  Même si je ne les connais pas
-  Rappelle-toi Barbara
-  N'oublie pas
-  Cette pluie sage et heureuse
-  Sur ton visage heureux
-  Sur cette ville heureuse
-  Cette pluie sur la mer
-  Sur l'arsenal
-  Sur le bateau d'Ouessant
-  Oh Barbara
-  Quelle connerie la guerre
-  Qu'es-tu devenue maintenant
-  Sous cette pluie de fer
-  De feu d'acier de sang
-  Et celui qui te serrait dans ses bras
-  Amoureusement
-  Est-il mort disparu ou bien encore vivant
-  Oh Barbara
-  Il pleut sans cesse sur Brest
-  Comme il pleuvait avant
-  Mais ce n'est plus pareil et tout est abimé
-  C'est une pluie de deuil terrible et désolée
-  Ce n'est même plus l'orage
-  De fer d'acier de sang
-  Tout simplement des nuages
-  Qui crèvent comme des chiens
-  Des chiens qui disparaissent
-  Au fil de l'eau sur Brest
-  Et vont pourrir au loin
-  Au loin très loin de Brest
-  Dont il ne reste rien. "
-
-  test3 = "Alice was published in 1865, three years after Charles Lutwidge Dodgson and the Reverend Robinson Duckworth rowed in a
-  boat, on 4 July 1862 [4] (this popular date of the golden afternoon [5] might be a confusion or even another Alice-tale, for that
-  particular day was cool, cloudy and rainy [6] ), up the Isis with the three young daughters of Henry Liddell (the Vice-Chancellor ofOxford University and Dean of Christ Church): Lorina Charlotte Liddell (aged
-  13, born 1849) (Prima in the book's prefatory verse); Alice Pleasance Liddell
-  (aged 10, born 1852) (Secunda in the prefatory verse); Edith Mary Liddell
-  (aged 8, born 1853) (Tertia in the prefatory verse). [7]
-  The journey began at Folly Bridge near Oxford and ended five miles away in the
-  village of Godstow. During the trip Charles Dodgson told the girls a story that
-  featured a bored little girl named Alice who goes looking for an adventure. The
-  girls loved it, and Alice Liddell asked Dodgson to write it down for her. He
-  began writing the manuscript of the story the next day, although that earliest
-  version no longer exists. The girls and Dodgson took another boat trip a month
-  later when he elaborated the plot to the story of Alice, and in November he
-  began working on the manuscript in earnest."
-
-  it "should detect korean" do
-    subject.detect(test).should eq("ko")
-  end
-  it "should detect french" do
-    subject.detect(test2).should eq("fr")
-  end
-  it "should detect english" do
-    subject.detect(test3).should eq("en")
+  it "should detect supported languages" do
+    json = File.read(File.expand_path("./data/fixtures.json", __DIR__))
+    fixtures = JSON.parse(json)
+    fixtures.as_h.each do |_, v|
+      fixture = v["fixture"].as_s
+      iso6393 = v["iso6393"].as_s
+      subject.detect(fixture).should eq(iso6393)
+    end
   end
 end
--- src/cadmium/language_detector/language_detector.cr
+++ src/cadmium/language_detector/language_detector.cr
@@ -1,61 +1,67 @@
 require "json"
+require "cadmium_ngrams"
+
 require "./language"
 
 module Cadmium
-  # Cadmium::LanguageDetector is a Language Identification algorithm which identifies up to 400 different languages.
+  # `Cadmium::LanguageDetector` is a Language Identification algorithm which identifies up to 400 different languages.
   class LanguageDetector
     include Language
+
     @@lang_data = LanguageData.new
-    @@trigrams_data : Hash(String, Hash(String, String)) = @@lang_data.trigrams
-    @@expressions : Hash(String, Regex) = @@lang_data.expressions
-    @@languages = Hash(String, Array(String)).new             # TODO Symbolize keys
-    @@iso_hash : Hash(String, String) = IsoCode3To1.new.codes # TODO Symbolize keys + values
-    @@whitelist : Tuple(String) = {""}                        # TODO Symbolize
-    @@blacklist : Tuple(String) = {""}                        # TODO Symbolize
+    @@ngrams = Cadmium::NGrams.new
+
+    @trigrams_data : Hash(String, Hash(String, String))
+    @expressions : Hash(String, Regex)
+    @languages = Hash(String, Array(String)).new
+    @iso_hash : Hash(String, String)
+    @whitelist : Array(String)
+    @blacklist : Array(String)
+
     # A LanguageDetector object can be initiampized with a whitelist and/or blacklist of languages. Those lists have to be Tuple of String.
-    def initialize(whitelist = @@whitelist, blacklist = @@blacklist)
-      @@whitelist = whitelist
-      @@blacklist = blacklist
-      @@trigrams_data.values.each do |languages|
+    def initialize(@whitelist = [] of String, @blacklist = [] of String)
+      @trigrams_data = @@lang_data.trigrams
+      @expressions = @@lang_data.expressions
+      @iso_hash = IsoCode3To1.new.codes
+
+      @trigrams_data.values.each do |languages|
         languages.each do |language, model|
-          @@languages[language] = model.split('|') if (whitelist.includes?(@@iso_hash.fetch(language, language)) || !blacklist.includes?(@@iso_hash.fetch(language, language)))
+          if whitelist.includes?(@iso_hash.fetch(language, language)) ||
+              !blacklist.includes?(@iso_hash.fetch(language, language))
+            @languages[language] = model.split('|')
+          end
         end
       end
     end
 
     private def trigrams_and_value(text : String) : Hash(String, Int32)
-      text_without_punctuation = text.downcase.delete('\n').gsub(/[\x{0021}-\x{0040}]/, "")
-      trigrams_array = Array(String).new
-      text_without_punctuation.scan(/.{3}/).each { |trigram| trigrams_array << trigram[0] }
+      text_without_punctuation = text.strip.gsub(/[[:punct:][:cntrl:]]/, "")
+      trigrams_array = text_without_punctuation.scan(/.{3}/).map { |trigram| trigram[0] }
       trigrams_count_hash = trigrams_array.tally
-      sorted_trigrams_hash = Hash(String, Int32).new # All of this could be replaced by a one liner if Crystal supported sort_by for Hash...
-      trigrams_count_hash.values.sort_by { |values| -values }.each do |value|
-        sorted_trigrams_hash[trigrams_count_hash.key_for(value)] = value
-      end
-      sorted_trigrams_hash
+      trigrams_count_hash.to_a.sort_by { |(k, values)| -values }.to_h
     end
 
     # Returns a two letters string corresponding to the ISO-868-1 code of the detected language.
     def detect(text : String) : String
       result = detect_all(text).keys[0]
-      @@iso_hash.fetch(result, result)
+      # @iso_hash.fetch(result, result)
     end
 
-    # Returns an Hash of two letters string corresponding to the ISO-868-1 code of the detected language mapped to their probability.
+    # Returns an Hash of two letters string corresponding to the ISO-868-1 code of the detected
+    # language mapped to their probability.
     def detect_all(text : String) : Hash(String, Float64)
-      expression = get_top_expression(text, @@expressions)
-      return {expression.keys[0] => 1.0} unless @@trigrams_data.keys.includes?(expression.keys[0])
-      normalize(text, get_distances(trigrams_and_value(text), @@languages))
+      expression = get_top_expression(text, @expressions)
+      return {expression.keys[0] => 1.0} unless @trigrams_data.keys.includes?(expression.keys[0])
+      distances = get_distances(trigrams_and_value(text), @languages)
+      normalize(text, distances)
     end
 
     private def normalize(text : String, distances : Hash(String, Int32)) : Hash(String, Float64)
       min = !distances.values[1..].empty? ? distances.values[1] : 1
       max = (text.size + 1) * 300 - min
-      distances_float = Hash(String, Float64).new
-      distances.each do |string, distance|
-        distances_float[string] = (1 - (distance - min) / max).to_f || 0.0
-      end
-      distances_float
+      distances.map do |string, distance|
+        {string, (1 - (distance - min) / max).to_f || 0.0}
+      end.to_h
     end
 
     private def get_occurence(text : String, expression : Regex) : Int32
@@ -78,11 +84,10 @@ module Cadmium
     end
 
     # Calculate the distances between an array of trigrams and multiple trigrams dictionaries (languages from data.json)
-    private def get_distances(text_trigrams : Hash(String, Int32), languages : Hash(String, Array(String)) = @@languages) : Hash(String, Int32)
-      distances = Hash(String, Int32).new
-      languages.each do |language, language_trigrams|
-        distances[language] = get_distance(text_trigrams, language_trigrams)
-      end
+    private def get_distances(text_trigrams : Hash(String, Int32), languages : Hash(String, Array(String)) = @languages) : Hash(String, Int32)
+      distances = languages.map do |language, language_trigrams|
+        {language, get_distance(text_trigrams, language_trigrams)}
+      end.to_h
 
       sorted_distances = Hash(String, Int32).new # All of this could be replaced by a one liner if Crystal supported sort_by for Hash...
       distances.values.sort_by { |values| values }.each do |value|
